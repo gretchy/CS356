0048066ef7cbd1ac48784c2538a93854ffabad0a
Gretchen Lai <glai@konicom.com>
2017-10-13 13:45:31 -0700

    basic classes needed

---
+++ Question.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Question {
+	
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ iVoteService.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class iVoteService {
+	
+	public static void main(String[] args) {
+		
+		
+	}
+}
